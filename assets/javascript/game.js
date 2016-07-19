59a185f295bfe46d1ae7ce8990ee9c78a34e0785
Colin McDaniel <colinmcdaniel@MacBook-Pro.local>
2016-07-19 12:45:09 -0700

    New pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,64 @@
-// Shuffles elements by replacing each element with a random element (Durstenfeld method)
+var wins = 0;
+var losses = 0;
+var wordNumber = 0;
+var displayedWord = "";
+var guessesRemaining = 12;
+var lettersGuessed = [];
+var displayedGuesses = "";
+var movies = [
+	'spotlight',
+	'spectre',
+	'birdman',
+	'whiplash',
+	'boyhood',
+	'interstellar',
+	'gravity',
+	'frozen',
+	'her',
+	'argo',
+	'skyfall',
+	'hugo',
+	'inception',
+	'avatar',
+	'up',
+	'juno',
+	'crash',
+	'ray',
+	'chicago',
+	'adaptation',
+	'shrek',
+	'gladiator',
+	'traffic',
+	'titanic',
+	'fargo',
+	'braveheart',
+	'speed',
+	'jfk',
+	'misery',
+	'alien',
+	'casablanca',
+	'goldfinger',
+	'jaws',
+	'nashville',
+	'platoon',
+	'rocky',
+	'witness'
+	];
+var introOut = document.getElementById("intro");
+var winsTitleOut = document.getElementById("wins-title");
+var winsOut = document.getElementById("wins");
+var lossesTitleOut = document.getElementById("losses-title");
+var lossesOut = document.getElementById("losses");
+var wordTitleOut = document.getElementById("word-title");
+var wordOut = document.getElementById("word");
+var numGuessesTitleOut = document.getElementById("num-guesses-title");
+var numGuessesOut = document.getElementById("num-guesses");
+var lettersTitleOut = document.getElementById("letters-title");
+var lettersOut = document.getElementById("letters");
+var leftImage = document.getElementById("left-image");
+
+
+// Shuffles elements by replacing each element with a random element
 function shuffleArray(arr) {
     for (var i = arr.length - 1; i > 0; i--) {
         var randomIndex = Math.floor(Math.random() * (i + 1));
@@ -17,70 +77,27 @@ function inArray(str,arr) {
     return false;
 }
 
-// Sets possible movies in a random order (for every new game)
-var movies = ['scream',
-	'halloween',
-	'psycho',
-	'nosferatu',
-	'jaws',
-	'poltergeist',
-	'alien',
-	'carrie',
-	'frankenstein',
-	'dracula'];
 
+// Randomly orders movies for each new game
 movies = shuffleArray(movies);
-
-// Defining variables
-var wins = 0;
-var losses = 0;
-var wordNumber = 0;
-var displayedWord = "";
-var guessesRemaining = 12;
-var lettersGuessed = [];
-var displayedGuesses = "";
 
 // Sets displayed word to correct number of characters
 for(i = 0; i < movies[wordNumber].length; i++)
 	displayedWord = displayedWord + "_";
 
 // Initial screen output
-var introOut = document.getElementById("intro");
 introOut.innerHTML = "Press any key to get started!";
-
-var winsTitleOut = document.getElementById("wins-title");
 winsTitleOut.innerHTML = "Wins";
-
-var winsOut = document.getElementById("wins");
 winsOut.innerHTML = wins;
-
-var lossesTitleOut = document.getElementById("losses-title");
 lossesTitleOut.innerHTML = "Losses";
-
-var lossesOut = document.getElementById("losses");
 lossesOut.innerHTML = losses;
-
-var wordTitleOut = document.getElementById("word-title");
 wordTitleOut.innerHTML = "Current word";
-
-var wordOut = document.getElementById("word");
 wordOut.innerHTML = displayedWord;
-
-var numGuessesTitleOut = document.getElementById("num-guesses-title");
 numGuessesTitleOut.innerHTML = "Guesses remaining";
-
-var numGuessesOut = document.getElementById("num-guesses");
 numGuessesOut.innerHTML = guessesRemaining;
-
-var lettersTitleOut = document.getElementById("letters-title");
 lettersTitleOut.innerHTML = "Letters already guessed";
-
-var lettersOut = document.getElementById("letters");
 lettersOut.innerHTML = displayedGuesses;
 
-var leftImage = document.getElementById("left-image");
-
-// Captures key up
 document.onkeyup = function(event) {
 
 	// Determines which letter the user pressed and makes it lowercase
@@ -90,67 +107,86 @@ document.onkeyup = function(event) {
 	if(!inArray(letter,lettersGuessed) && letter.match(/[a-z]/i)){
 		lettersGuessed.push(letter);
 
+		// If correct guess...
 		if(movies[wordNumber].indexOf(letter) >= 0){
+
+			// Replace "_" with guessed letter
 			for(i = 0; i < movies[wordNumber].length; i++)
 				if(letter == movies[wordNumber].charAt(i)){
 					displayedWord = displayedWord.substr(0, i) + letter + displayedWord.substr(i + 1);
 			}
-		}
-		else{
-			guessesRemaining--;
-			leftImage.src = "assets/images/" + guessesRemaining + ".jpg";
 
-			if(guessesRemaining == 0){
-				losses++;
-				wordNumber = 0;
+			// If won game...
+			if(movies[wordNumber] == displayedWord){
+				wins++;
+
+				// Display movie poster
+				leftImage.src = "assets/images/" + movies[wordNumber] + ".jpg";
+
+				// Reset initial variables
 				displayedWord = "";
 				guessesRemaining = 12;
 				lettersGuessed = [];
 				displayedGuesses = "";
 
-				// Sets displayed word to correct number of characters
-				if(wordNumber != movies.length){
-					for(i = 0; i < movies[wordNumber].length; i++)
-						displayedWord = displayedWord + "_";
-				}
+				// If there are more unplayed words...
+				if(wordNumber < movies.length - 1)
+					wordNumber++;
 
-				lossesOut.innerHTML = losses;
-			}
-			else{
-				displayedGuesses = displayedGuesses + letter;
+				// If user has played all words...
+				else if(wordNumber == movies.length - 1)
+					wordNumber = 0;
+
+				// Resets displayed word to hidden
+				for(i = 0; i < movies[wordNumber].length; i++)
+					displayedWord = displayedWord + "_";
 			}
 		}
 
-		wordOut.innerHTML = displayedWord;
-		numGuessesOut.innerHTML = guessesRemaining;
-		lettersOut.innerHTML = displayedGuesses;
+		// If incorrect guess...
+		else{
+			guessesRemaining--;
 
-		if(movies[wordNumber] == displayedWord && wordNumber < movies.length - 1){
-			wins++;
-			wordNumber++;
-			displayedWord = "";
-			guessesRemaining = 12;
-			lettersGuessed = [];
-			displayedGuesses = "";
+			// Display next hangman picture
+			leftImage.src = "assets/images/" + guessesRemaining + ".jpg";
 
-			// Sets displayed word to correct number of characters
-			if(wordNumber != movies.length){
+			// If lost game...
+			if(guessesRemaining == 0){
+				losses++;
+
+				// Reset initial variables
+				displayedWord = "";
+				guessesRemaining = 12;
+				lettersGuessed = [];
+				displayedGuesses = "";
+
+				// If there are more unplayed words...
+				if(wordNumber < movies.length - 1)
+					wordNumber++;
+
+				// If user has played all words...
+				else if(wordNumber == movies.length - 1)
+					wordNumber = 0;
+
+				// Resets displayed word to hidden
 				for(i = 0; i < movies[wordNumber].length; i++)
 					displayedWord = displayedWord + "_";
 			}
 
-			winsOut.innerHTML = wins;
-			wordOut.innerHTML = displayedWord;
-			numGuessesOut.innerHTML = guessesRemaining;
-			lettersOut.innerHTML = displayedGuesses;
+			// If haven't lost, display guessed letter
+			else{
+				if(displayedGuesses.length == 0)
+					displayedGuesses = displayedGuesses + letter;
+				else
+					displayedGuesses = displayedGuesses + "  " + letter;
+			}
 		}
-		else if(movies[wordNumber] == displayedWord && wordNumber == movies.length - 1){
-			wins++
 
-			winsOut.innerHTML = wins;
-
-			// USE APPENDCHILD FOR THIS AND FOR LOSES TOO
-			introOut.innerHTML = "YOU WIN!!! Refresh page for a new game.";
-		}
+		// Update screen
+		winsOut.innerHTML = wins;
+		lossesOut.innerHTML = losses;
+		wordOut.innerHTML = displayedWord;
+		numGuessesOut.innerHTML = guessesRemaining;
+		lettersOut.innerHTML = displayedGuesses;
 	}
 }
